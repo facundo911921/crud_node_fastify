3709f4def1cfec69ee0d5301c40a9de0d100bf80
Rafael Facundo <newrafaelcosta2000@gmail.com>
2024-12-23 17:47:15 -0300

    update gitignore

--- db.js
+++ db.js
@@ -2,7 +2,5 @@ import 'dotenv/config';
 import postgres from "postgres";
 
 
-
-// const { PGHOST, PGUSER, PGPASSWORD, PGDATABASE, PGPORT } = process.env;
 const URL= process.env.URL;
 export const sql = postgres(URL, {ssl: 'require'});
