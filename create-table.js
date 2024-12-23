fda925ac9e0a23f5a8e5107268bc39f3138fb31f
Rafael Facundo <newrafaelcosta2000@gmail.com>
2024-12-22 21:39:38 -0300

    update to postgres database

--- create-table.js
+++ create-table.js
@@ -1,11 +1,14 @@
 import { sql } from './db.js';
 
+// sql`DROP TABLE IF EXISTS vídeos;`.then(() => {console.log('Tabela deletada com sucesso')});
+
 sql`
     CREATE TABLE vídeos (
+        id    TEXT PRIMARY KEY,
         title	TEXT,
         description	TEXT,
         duration	INTEGER
     );
 `.then(() => {
     console.log('tabela criada com sucesso');
-})
+});
